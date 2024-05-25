1a91eaf03a9a27c5221ad8d4c43ff0ca7f1b0cbe
qwertuiopy <81052542+qwertuiopy@users.noreply.github.com>
2024-05-25 17:13:40 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,3 @@
+function collectbadge() {
+    alert("Congratulations! You have collected the Balsa Badge. The Balsa tree, or Ochroma pyramidale is native to the Americas and known for its soft wood, which is used in woodworking.");
+}
