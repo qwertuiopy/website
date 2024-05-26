60df08a5a8531fcdfcab8498df7479638984952d
qwertuiopy <81052542+qwertuiopy@users.noreply.github.com>
2024-05-26 13:42:27 -0400

    added fish badge

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
-function collectbadge() {
+function collectbalsabadge() {
     alert("Congratulations! You have collected the Balsa Badge. The Balsa tree, or Ochroma pyramidale is native to the Americas and known for its soft wood, which is used in woodworking.");
+}
+function collectfishbadge() {
+    alert("Congratulations! You have collected the Special Fish Badge. Atlantic Cod can live for more than 20 years, and range from Greenland to North Carolina. They have a whisker like organ called a barbel on the bottom of their head.");
 }
