1c9ef49d1ae6385a7796384552bcc632f37b52b8
qwertuiopy <81052542+qwertuiopy@users.noreply.github.com>
2024-05-26 22:59:38 -0400

    trying to debug form

--- form.js
+++ form.js
@@ -1,5 +1,5 @@
 function newpost() {
     var name = document.getElementById("username");
     var message = document.getElementById("message");
-    document.getElementById("messages").innerHTML = message+" - "+name
+    document.getElementById("post").innerHTML = message+" - "+name
 }
